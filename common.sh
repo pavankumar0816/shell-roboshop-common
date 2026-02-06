f7cfbd4996f5f3b8e406bccc2ad65561363b214b
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-06 22:55:44 +0530

    Updated Common.sh file

--- common.sh
+++ common.sh
@@ -61,6 +61,15 @@ java_setup(){
 
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Python"
+
+    cd /app 
+    pip3 install -r requirements.txt &>>$LOGS_FILE
+       VALIDATE $? "Installing Dependencies"
+}
+
 app_setup(){
     id roboshop &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
---
+++ payment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+check_root
+app_setup
+python_setup
+systemd_setup
+
+print_total_time
