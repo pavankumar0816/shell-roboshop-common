2583cb33f9a3a900cca4219462e654229e575b08
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-06 21:49:04 +0530

    Updated Common.sh file

--- catalogue.sh
+++ catalogue.sh
@@ -11,13 +11,15 @@ systemd_setup
 
 # Loading data into Mongodb
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>> $LOGS_FILE
 
 INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")') # Mongodb shell command to check db exists ot not db.getMongo().getDBNames() will list all the dbs and indexOf("catalogue") will check if catalogue db is there or not if it is there it will return index number 1 if not it will return -1
 
 if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOGS_FILE
     VALIDATE $? "Loading Products"
 else
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Products are already loaded ... $Y Skipping $N"  
 fi
+
+print_total_time
---
+++ redis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+
+if command -v redis-server &>/dev/null && redis-server -v | grep -q "v=7"; then
+    echo -e "Already Installed ... $Y Skipping $N" | tee -a $LOGS_FILE
+else
+    dnf module disable redis -y &>> $LOGS_FILE
+    VALIDATE $? "Disabling Default Redis Module"
+    dnf module enable redis:7 -y &>> $LOGS_FILE
+    VALIDATE $? "Enabling Redis 7 Module"
+    dnf install redis -y &>> $LOGS_FILE
+    VALIDATE $? "Installing Redis"
+fi
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote Connections"
+
+systemctl enable redis &>> $LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enable and Started Redis"
+
+print_total_time
