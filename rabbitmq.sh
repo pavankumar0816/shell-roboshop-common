9778762cefec986a5c694624610875249aab8e48
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-06 22:41:15 +0530

    Updated Common.sh file

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,15 +5,15 @@ source ./common.sh
 check_root
 
 cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>> $LOGS_FILE
-validate $? "Copying RabbitMQ Repo"
+VALIDATE $? "Copying RabbitMQ Repo"
 
 dnf install rabbitmq-server -y &>> $LOGS_FILE
-validate $? "Installing RabbitMQ Server"
+VALIDATE $? "Installing RabbitMQ Server"
 
 systemctl enable rabbitmq-server &>> $LOGS_FILE
 systemctl start rabbitmq-server
-validate $? "Enable and started RabbitMQ Server"
+VALIDATE $? "Enable and started RabbitMQ Server"
 
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-validate $? "Created RabbitMQ User and Set Permissions"
+VALIDATE $? "Created RabbitMQ User and Set Permissions"
--- shipping.sh
+++ shipping.sh
@@ -8,16 +8,16 @@ app_setup
 java_setup
 
 dnf install mysql -y 
-validate $? "Installing Mysql Client"
+VALIDATE $? "Installing Mysql Client"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOGS_FILE
 if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
-    validate $? "Loading Shipping Schema"
+    VALIDATE $? "Loading Shipping Schema"
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>> $LOGS_FILE
-    validate $? "Loading Shipping Schema and User Data"
+    VALIDATE $? "Loading Shipping Schema and User Data"
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>> $LOGS_FILE
-    validate $? "Loading Shipping Master Data"
+    VALIDATE $? "Loading Shipping Master Data"
 else 
     echo -e "Shipping Database already exists ... $Y Skipping $N"
 fi
